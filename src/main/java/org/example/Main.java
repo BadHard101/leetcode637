3e01884862c9dd8bf05e12ac06eb566e37e4bef8
BadHard <BadHard101@yandex.ru>
2024-01-14 17:49:20 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.List;
+
+public class Main {
+    public List<Double> averageOfLevels(TreeNode root) {
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
